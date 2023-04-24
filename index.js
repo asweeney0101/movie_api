2bd5ceaceafb2ce2800281631bbc0d454a6afbb3
Alex Sweeney <ajsweeney2324@gmail.com>
2023-04-23 21:52:53 -0500

    added express and morgan

---
+++ index.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path'),
+    app = express();
+
+
+app.use(express.static('public'));    
+app.use(morgan('common'));
+
+// Logging when the API is accessed
+const accessLog = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+app.use(morgan('combined', {stream: accessLog}));
+
+
+app.get('/movies', (req, res) => {
+   // Filler list for now
+    let myMovies = [
+            {
+            title: 'Book of Eli',
+            director: 'The Hughes Brothers'
+            },
+            {
+            title: 'Inception',
+            director: 'Christopher Nolan'
+            },
+            {
+            title: 'The Departed',
+            director: 'Martin Scorsese'
+            },
+            {
+            title: 'Lord of the Rings: Return of the King',
+            director: 'Peter Jackson'
+            },
+            {
+            title: 'V for Vendetta',
+            director: 'James McTeigue'
+            },
+            {
+            title: 'A Promising Young Woman',
+            director: 'Emerald Fennell'
+            },
+            {
+            title: 'The Dark Knight',
+            director: 'Christopher Nolan'
+            },
+            {
+            title: 'Forrest Gump',
+            director: 'Robert Zemeckis'
+            },
+            {
+            title: 'The Incredibles',
+            director: 'Brad Bird'
+            },
+            {
+            title: 'Interstellar',
+            director: 'Christopher Nolan'
+            }   
+        ];
+    res.json(myMovies);
+})
+
+app.get('/', (req,res) => {
+    res.send('Welcome to my API!');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(' Something broke :( ');
+  });
+
+ 
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
+
+
+
+
