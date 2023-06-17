ded81e15d072c0d4904c1878e8d2c39d302588c8
Alex Sweeney <ajsweeney2324@gmail.com>
2023-06-17 12:26:27 -0500

    updated ports for heroku commit

--- server.js
+++ server.js
@@ -64,91 +64,16 @@ app.use((err, req, res, next) => {
     res.status(500).send(' Something broke :( ');
   });
 
-// connect to port 8080
- app.listen(8080, () => {
-   console.log('App is listening on port 8080');
+// connect to ports
+const port = process.env.PORT || 8080; 
+app.listen(port, '0.0.0.0', () => {
+   console.log('App is listening on port ' + port);
  });
 
 // Default greeting message
 app.get('/', (req,res) => {
     res.status(200).send('Welcome to the myFlix API!');
 });
-
-// Filler movie list for now
-let movies = [
-   
-    {
-        Title: 'Inception', 
-        Description: 'The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets.',
-        Genre: {
-            Name: 'science fiction',
-            Description: 'Science fiction (or sci-fi) is a film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, dinosaurs, interstellar travel, time travel, or other technologies.'
-        },
-        Director: 
-        {
-            Name: 'Christopher Nolan',
-            bio: 'Christopher Edward Nolan is a British-American filmmaker who is known for his Hollywood blockbusters with complex storytelling, Nolan is considered a leading filmmaker of the 21st century.',
-            Birthyear: '1970',
-            Deathyear: 'present'
-        },
-        imageUrl: 'https://pixabay.com/images/id-3265473/',
-        year: '2010',
-        featured: 'yes'
-    },
-   
-    {
-        title: 'The Lion King',
-        description: 'A young lion prince flees his kingdom after the murder of his father. Years later, he returns to reclaim his throne.',
-        director: {
-          firstName: 'Roger',
-          lastName: 'Allers',
-          bio: 'Roger Allers is an American film director, screenwriter, storyboard artist, animator and voice actor. He is best known for co-directing the Disney animated feature The Lion King (1994).',
-          dateOfBirth: 1949,
-          dateOfDeath: undefined
-        },
-        genres: [
-          {
-            name: 'Animation',
-            description: 'Animated films are ones in which individual drawings, paintings, or illustrations are photographed frame by frame (stop-frame cinematography).'
-          },
-          {
-            name: 'Adventure',
-            description: 'Adventure films are often set in an historical period and may include adapted stories of historical or literary adventure heroes, kings, battles, rebellion, or piracy.'
-          },
-          {
-            name: 'Drama',
-            description: 'Drama films are a genre that relies on the emotional and relational development of realistic characters. They often feature intense character development, and sometimes rely on tragedy to evoke an emotional response from the audience.'
-          }
-        ],
-        imageURL: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wx3wpNh4LhRJ3h6yN3vSGPVepuo.jpg',
-        featured: true
-      },
-
-    {
-        Title: 'The Lord of the Rings: The Return of the King', 
-        Description: 'Continuing the plot of the previous film, Frodo, Sam and Gollum are making their final way toward Mount Doom in Mordor in order to destroy the One Ring, unaware of Gollum\'s true intentions, while Merry, Pippin, Gandalf, Aragorn, Legolas, Gimli and the rest are joining forces together against Sauron and his legions in Minas Tirith.',
-        Genre: 
-        {
-            Name: 'fantasy',
-            Description: 'Fantasy films are films that belong to the fantasy genre with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds.'
-        },
-        Director: 
-        {
-            Name: 'Peter Jackson',
-            bio: 'Sir Peter Robert Jackson is a New Zealand film director, screenwriter and producer.',
-            Birthyear: '1961',
-            Deathyear: 'present'
-        },
-        imageUrl: 'https://pixabay.com/images/id-2021410/',
-        year: '2003',
-        featured: 'yes'
-    },
-
-
-
-
-];
-
 
 
 // Create  // Create
