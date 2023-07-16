c78172e00093a17d7ad9f85e46f7529ad960550d
Alex Sweeney <ajsweeney2324@gmail.com>
2023-07-16 14:15:33 -0500

    added back passport

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/documentation', (req,res) => {
     res.sendFile('public/Documentation.html', {root: __dirname});     
 });
 
-app.get('/users',
+app.get('/users', passport.authenticate('jwt', { session: false }),
  (req, res) => {
     Users.find()
       .then((users) => {
@@ -150,7 +150,7 @@ app.get('/users',
       });
   });
 
-  app.get('/movies',  
+  app.get('/movies', passport.authenticate('jwt', { session: false }),
    (req, res) => {
     Movies.find()
       .then((movies) => {
