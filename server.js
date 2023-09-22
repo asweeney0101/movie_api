32aa051491a5b79b625c0c4533f93bfc4fb27e50
Alex Sweeney <ajsweeney2324@gmail.com>
2023-09-21 20:14:05 -0500

    updated CORS for netlify

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:1234' ];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:1234', 'https://main--my-flix-ajs.netlify.app/' ];
 
 app.use(cors({
   origin: (origin, callback) => {
